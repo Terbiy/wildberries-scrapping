8736fda52316703f27333428237c127642484105
German Tebiev <terbiyarchitect@gmail.com>
2021-05-10 06:57:55 +0300

    Исправлено падение программы при обработке товара с отстствующей ценой

--- src/wildberries_scrapping/data_extractors.clj
+++ src/wildberries_scrapping/data_extractors.clj
@@ -1,12 +1,20 @@
-(ns wildberries-scrapping.data-extractors)
+(ns wildberries-scrapping.data-extractors
+  (:require [clojure.pprint :refer [pprint]]))
 
 (defn get-comments-number [good] (:feedbacks good))
 
 (defn get-rating [good] (:rating good))
 
-(defn get-price [good] (/ (:salePriceU good) 100))
+(defn get-price
+  [good]
+  (let [sale-price (:salePriceU good)]
+    (if sale-price
+      (/ sale-price 100)
+      (do (println "У следующего товара отсутствует цена, вместо неё будет 0:")
+          (pprint good)
+          0))))
 
-(defn get-discount [good] (:sale good))
+(defn get-discount [good] (or (:sale good) 0))
 
 (defn get-name [good] (:name good))
 
